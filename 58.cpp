03a74d8b8501e9cbc44f17b6b3e68b2f86f6e76c
RahulDhi123 <rahul.ee.25@nitj.ac.in>
2026-07-22 00:29:06 +0530

    adding

--- 58.cpp
+++ 58.cpp
@@ -1,1 +1,38 @@
 // REVERSE A STRING
+
+#include<iostream>
+using namespace std;
+
+int length(char name[]){
+     int count=0;
+    int i=0;
+    while(name[i]!='\0'){
+        count++;
+        i++;
+    }
+    return count;
+}
+
+
+void reverse(char name[],int size){
+    int st=0;
+    int end=size-1;
+    while(st<end){
+        swap(name[st],name[end]);
+        st++;
+        end--;
+    }
+}
+int main(){
+
+    char name[30];
+    cout<<"enter a string:\n";
+    cin>>name;
+
+    int len=length(name);
+    reverse(name,len);
+
+
+    cout<<name;
+
+}

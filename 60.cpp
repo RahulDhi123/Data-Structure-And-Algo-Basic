4715d2445f18a47f4c6e387fc1add33f1508fe5a
RahulDhi123 <rahul.ee.25@nitj.ac.in>
2026-07-22 01:19:03 +0530

    adding......

---
+++ 60.cpp
@@ -0,0 +1,25 @@
+// VALID PALINDROME (LEETCODE)
+
+#include<iostream>
+using namespace std;
+int main(){
+    char n[30];
+    cout<<"enter a string:\n";
+    cin>>n;
+
+
+     int count=0;
+    int i=0;
+    while(n[i]!='\0'){
+        count++;
+        i++;
+    }
+
+    int st=0;
+    int end=count-1;
+
+    while(st<end){
+
+        
+    }
+}

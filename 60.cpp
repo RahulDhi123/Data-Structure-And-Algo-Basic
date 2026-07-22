84362ffe938a154cc5349697454a37f3807bd64f
RahulDhi123 <rahul.ee.25@nitj.ac.in>
2026-07-22 12:28:35 +0530

    adding.....

--- 60.cpp
+++ 60.cpp
@@ -1,25 +1,64 @@
-// VALID PALINDROME (LEETCODE)
+// VALID PALINDROME(LEETCODE)
 
 #include<iostream>
 using namespace std;
+
+
+bool isalpha(char s){
+    if(s>='A' && s<='Z' ||  s>='a' && s<='z' || s>'0' && s<'9'){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 int main(){
-    char n[30];
+    char s[30];
     cout<<"enter a string:\n";
-    cin>>n;
+    cin>>s;
 
-
-     int count=0;
+      int count=0;
     int i=0;
-    while(n[i]!='\0'){
+    while(s[i]!='\0'){
         count++;
         i++;
     }
 
     int st=0;
     int end=count-1;
+    int pal=1;
+
 
     while(st<end){
+        if( isalpha(s[st])  &&  isalpha(s[end])){
+            if(tolower(s[st])!=tolower(s[end])){
+                pal=0;
+                break;
+            }
 
-        
+        }
+
+        if(isalpha(s[st]) && !(isalpha(s[end]))){
+            end--;
+        }
+
+
+        if(isalpha(s[end]) && !(isalpha(s[st]))){
+            st++;
+        }
+        else{
+            st++;
+            end--;
+        }
     }
+
+
+    if(pal){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+
 }
